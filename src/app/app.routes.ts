0265cf4eec190664e31b0a59b9f627063a0a56fb
Ali Haider <ha4219466@gmail.com>
2024-03-05 19:26:18 +0500

    add routing.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MasterComponent } from './shared/layouts/master/master.component';
+import { MessageComponent } from './pages/message/message.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MasterComponent,
+    children: [
+      { path: 'messages', component: MessageComponent },
+      { path: 'compose', component: MessageComponent },
+      { path: '', component: LoginComponent },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
+];
