ca42657c9320f7804a86d37456496e6f34fa7813
Ali Haider <ha4219466@gmail.com>
2024-03-08 21:20:30 +0500

    add master component

---
+++ src/app/shared/layouts/master/master.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+
+@Component({
+  selector: 'app-master',
+  standalone: true,
+  imports: [RouterModule, HeaderComponent, SidebarComponent],
+  templateUrl: './master.component.html',
+  styleUrl: './master.component.scss',
+})
+export class MasterComponent {}
