a1bb6af222071666ec9ba4a706de79a74b17cb21
Ali Haider <ha4219466@gmail.com>
2024-03-13 20:41:04 +0500

    new component

---
+++ src/app/shared/component/message-items/message-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-message-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './message-items.component.html',
+  styleUrl: './message-items.component.scss'
+})
+export class MessageItemsComponent {
+
+}
