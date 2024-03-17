a99020d3b5b0f3d3676a05598dd49a786bfc8eb0
Ali Haider <ha4219466@gmail.com>
2024-03-17 22:31:56 +0500

    add login component

---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CardComponent } from '../../shared/component/card/card.component';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {}
