c06f9de8676fe284e5e7116f9987245b37532512
Ali Haider <ha4219466@gmail.com>
2024-03-19 16:18:18 +0500

    update files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { MasterComponent } from './shared/layouts/master/master.component';
 import { MessageComponent } from './pages/message/message.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ComposeComponent } from './pages/compose/compose.component';
 
 export const routes: Routes = [
   {
@@ -10,7 +11,7 @@ export const routes: Routes = [
     component: MasterComponent,
     children: [
       { path: 'messages', component: MessageComponent },
-      { path: 'compose', component: MessageComponent },
+      { path: 'compose', component: ComposeComponent },
       { path: '', component: LoginComponent },
       { path: 'register', component: RegisterComponent },
     ],
